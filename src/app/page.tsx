4279fba91b25509ad2db9a3641b64db11f220b21
Levy Monteiro <levymonteiro268@gmail.com>
2024-05-21 16:43:21 -0300

    create Feature component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import Cart from '@/components/font-end/Cart';
+import Feature from '@/components/font-end/Feature';
 import Hero from '@/components/font-end/Hero';
 import Navbar from '@/components/font-end/Navbar';
-import Image from 'next/image';
 import { useState } from 'react';
 
 export default function Home() {
@@ -13,6 +13,7 @@ export default function Home() {
       <Navbar setShowCart={setShowCart} />
       {showCart && <Cart setShowCart={setShowCart} />}
       <Hero />
+      <Feature />
     </main>
   );
 }
